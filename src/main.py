5b9f76f683be8e2a61e9ef8b3b1d74ae1a979bd8
Youtong0826 <jimmy2007826@gmail.com>
2024-03-25 01:20:24 +0800

    removed old code hint

--- src/main.py
+++ src/main.py
@@ -9,10 +9,6 @@ async def on_ready():
     bot.log("I'm ready!")
     bot.log("database: \n", bot.database)
 
-# @bot.event
-# async def on_member_join(member: Member):
-#     bot.log("new member join:", member.name, "id:", member.id)
-
 if __name__ == "__main__":
     bot.load_extension("commands")
     bot.load_extension("events")
